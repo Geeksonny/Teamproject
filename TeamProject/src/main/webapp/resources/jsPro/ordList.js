d85029c5b6492bf09ce0b867322f79499dc7348c
cy <109007132+richm1123@users.noreply.github.com>
2022-09-27 17:26:18 +0900

    220927 업체페이지-상품등록 코드입력수정, 택배송장입력수정,마이쿠폰-페이징처리
    220927 업체페이지-상품등록 코드입력수정, 택배송장입력수정,마이쿠폰-페이징처리

--- TeamProject/src/main/webapp/resources/jsPro/ordList.js
+++ TeamProject/src/main/webapp/resources/jsPro/ordList.js
@@ -12,8 +12,8 @@ function delivNumberAdd() {
 			function() {
 
 				$(this).next().html(
-					"  <div><input placeholder='택배사' class='form-control' type='text' style='float:left; width:55%;' id='ordLDelivNumber' name='ordLDelivNumber' >"
-					+ "  <br><div class='form-group'><input placeholder='송장번호' class='form-control' type='text' style='float:left; width:55%' id='ordLDelivNumber' name='ordLDelivNumber' >"
+					"  <div><input placeholder='택배사' class='form-control' type='text' style='float:left; width:55%;' id='ordLDelivComp' name='ordLDelivComp' >"
+					+ "  <br><div class='form-group'><input placeholder='송장번호' class='form-control' type='text' style='float:left; width:55%' id='ordLDelivNum' name='ordLDelivNum' >"
 							+ "<button type='submit' class='btn btn-primary ' >" + "입력"+"</button></div>"
 
 						);
