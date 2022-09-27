1180f43a278e614ddfce6c97e604795d39902a25
cy <109007132+richm1123@users.noreply.github.com>
2022-09-27 17:23:24 +0900

    220927 업체페이지-상품등록 코드입력수정, 택배송장입력수정,마이쿠폰-페이징처리
    220927 업체페이지-상품등록 코드입력수정, 택배송장입력수정,마이쿠폰-페이징처리

--- TeamProject/src/main/java/com/itwillbs/service/MemberService.java
+++ TeamProject/src/main/java/com/itwillbs/service/MemberService.java
@@ -2,72 +2,52 @@ package com.itwillbs.service;
 
 import java.util.List;
 
-import com.itwillbs.domain.CompDTO;
+import com.itwillbs.domain.BoardDTO;
 import com.itwillbs.domain.CouponDTO;
-import com.itwillbs.domain.MemberDTO;
-import com.itwillbs.domain.OrderDTO;
+import com.itwillbs.domain.MypageDTO;
 import com.itwillbs.domain.OrderListDTO;
 import com.itwillbs.domain.PageDTO;
-import com.itwillbs.domain.PointDTO;
 import com.itwillbs.domain.ProdDTO;
 
-public interface MemberService {
-	// 유저 회원가입
-	public void insertMember(MemberDTO memberDTO) throws Exception;
+public interface MypageService {
 
-	// 유저 로그인
-	public MemberDTO userCheck(MemberDTO memberDTO);
-	public MemberDTO getMember(String userId);
-	public void loginCheck(MemberDTO memberDTO);
-	int updateEmailKey(MemberDTO memberDTO) throws Exception;
-	int updateEmailAuth(MemberDTO memberDTO) throws Exception;
-	int emailAuthFail(String userId) throws Exception;
+	void insertMypage(MypageDTO userId);
 
-	// 이메일 중복 검사
-	public MemberDTO checkUserEmail(String userEmail);
+	void boardCount(MypageDTO mypageDTO);
 
-	// 업체 회원가입
-	public void insertComp(CompDTO compDTO);
+	void boardsub(MypageDTO mypageDTO);
 
-	// 업체 로그인
-	public CompDTO compCheck(CompDTO compDTO);
+	void replysub(MypageDTO mypageDTO);
 
-	// 아이디 찾기
-	public String idSearch(MemberDTO memberDTO);
+	void replyCount(MypageDTO mypageDTO);
 
-	// 비밀번호 찾기
-	public String pwCheck(MemberDTO memberDTO);
-	public void updatePass(MemberDTO memberDTO) throws Exception;
+	void productLikesub(MypageDTO mypageDTO);
 
-	// 회원 정보 수정
-	public void modUser(MemberDTO memberDTO);
+	void productLike(MypageDTO mypageDTO);
 
-	// 비밀번호 변경
-	public void passMod(MemberDTO memberDTO) throws Exception;
 
-	// 회원 탈퇴
-	public void delUser(MemberDTO memberDTO);
+	MypageDTO mypageselect(MypageDTO mypageDTO);
 
-	// 휴면 계정 전환
-	public void changeStatus(MemberDTO memberDTO);
-	// 휴면 계정 체크
-	int statusCheck(String userId);
+	List<BoardDTO> getBoardList(BoardDTO boardDTO);
 
-	// 유저 리스트
-	List<MemberDTO> getUserList(PageDTO pageDTO);
-	int getUserCount();
-	// 유저 삭제
-	public void deleteUser(String userId);
+	int getBoardCount();
 
-	// 주문 리스트
-	public List<OrderListDTO> getOrderList(PageDTO pageDTO);
-	public int getOrderCount();
-	// 상품 리스트
-	public List<ProdDTO> getProductList(PageDTO pageDTO);
-	public int getProductCount();
-	// 배송 리스트
-	public List<OrderDTO> getOrderBList(PageDTO pageDTO);
-	public int getOrderBCount();
+	List<BoardDTO> getBoardLikeList(BoardDTO boardDTO);
+
+	List<ProdDTO> getPordLikeList(ProdDTO prodDTO);
+	// ���������� �ֹ����
+		List<OrderListDTO> getOrdList(PageDTO pageDTO);
+		// ���������� �ֹ���� ����
+		int getMyOrdListCount(PageDTO pageDTO);
+		// 마이페이지 쿠폰 리스트
+		List<CouponDTO> getMyCouponList(CouponDTO couponDTO);
+		// 마이페이지 쿠폰 등록
+		void insertMyCoupon(CouponDTO couponDTO);
+		// 마이페이지 페이징 처리
+		int getMyCouponListCount(CouponDTO couponDTO);
+
+
+
 
 
 
